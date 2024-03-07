88efb0a3eadb5ff5724eec0e765ef47910be5b35
redmoonligth <108595659+redmoonligth@users.noreply.github.com>
2024-03-06 22:10:28 -0300

    Create Música.java

---
+++ Música.java
@@ -0,0 +1,14 @@
+public class Musica{
+
+    public String titulo;
+    public float duracao;
+    public Pessoa compositor;
+
+
+    public void tocarMusica(){
+
+        System.out.println("tocando");
+    }
+
+
+}
